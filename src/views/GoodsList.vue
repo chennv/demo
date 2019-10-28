a524b47c672a8cb4a3107cf7cb6e2a49ad5464a0
chennv <18911692501@163.com>
2019-10-28 11:25:00 +0800

    路由修改

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
 <!--  嵌套路由-->
-  <router-link to="/goods/title">Title</router-link>
-  <router-link to="/goods/image">Image</router-link>
+  <router-link to="/goodslist/title">Title</router-link>
+  <router-link to="/goodslist/image">Image</router-link>
   <router-view></router-view>
 <!--  编程式路由-->
   <div @click="goToGoods">跳转到详情页</div>
